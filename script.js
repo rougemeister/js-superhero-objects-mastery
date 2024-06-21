ac8dfe0a8022b5e117c5e6cd51e77de15c1cbd81
kruzznitsch <rougemeister1@gmail.com>
2024-06-21 12:24:33 +0100

    Used forEach, map, or filter to manipulate an array of superheroes and supervillains

--- script.js
+++ script.js
@@ -76,8 +76,8 @@ batman.revealIdentity();
 
 
 /*****************************************************
- **** Extend the functionality of your superheroes
-      using prototypal inheritance                ****
+ **** Task: 4 Extend the functionality of your 
+      superheroes using prototypal inheritance    ****
  *****************************************************/
 
 // Creating a subclass for a SuperVillain
@@ -96,3 +96,38 @@ let darkseid = new SuperVillain("Darkseid", "None", ["Telepathy", "Telekenesis",
 darkseid.usePower('heat vision')
 darkseid.usePower("Telekenesis");
 darkseid.revealIdentity();
+
+
+/*****************************************************
+ ****   Task 5: Use forEach, map, or filter to    ****
+ manipulate an array of superheroes and supervillains.
+ *****************************************************/
+
+
+
+// Creating an array of superheroes and supervillains
+let characters = [
+    new Superhero("Superman", "Clark Kent", ["super strength", "flight", "x-ray vision"], "kryptonite"),
+    new Superhero("Batman", "Bruce Wayne", ["martial arts", "detective skills"], "none"),
+    new SuperVillain("Joker", "Unknown", ["genius-level intellect", "expert chemist"], "Batman"),
+    new SuperVillain("Lex Luthor", "Lex Luthor", ["genius-level intellect", "expert engineer"], "Superman"),
+    new SuperVillain("Darkseid", "None", ["Telepathy", "Telekenesis", 'heat vision'], "Radion")
+];
+
+//forEach to log each character's name
+characters.forEach(character => {
+    console.log(character.name);
+});
+
+// Using map to create an array of characters' secret identities
+let secretIdentities = characters.map(character => character.secretIdentity);
+console.log(secretIdentities);
+
+//Using filter to get all supervillains
+const superVillains = characters.filter(character => character instanceof SuperVillain)
+console.log(superVillains)
+
+
+// Using filter to get genius supervillains
+let geniusSupervillains= superVillains.filter(character => character.powers.includes('genius-level intellect'));
+console.log(geniusSupervillains);
